cc7f5740db8be9f9be6de543e64b09868691d148
Shay Wagner <srwagner916@gmail.com>
2021-06-18 15:42:55 -0500

    added associations to /models/index -- User.hasMany(Comment) |  Comment.belongsTo(User) | Post.hasMany(Comment) | Comment.belongsTo(Post) --

--- models/index.js
+++ models/index.js
@@ -5,9 +5,13 @@
 ///||||       as you go          ||||\|
 //////|||||||||||||||||||||||||||||||||
 
+// require models
 const User = require('./User');
 const Post = require('./Post');
+const Comment = require('./Comment');
 
+// User and Post associations
+//=====================================
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
@@ -18,4 +22,28 @@ Post.belongsTo(User, {
   onDelete: 'SET NULL'
 });
 
-module.exports = { User, Post };
+//  User and Comment associations
+//=====================================
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+// Post and Comment associations
+//=====================================
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+//=====================================
+module.exports = { User, Post, Comment };
