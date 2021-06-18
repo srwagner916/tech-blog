681b691f6cbad8ae8c3a136a01328e479a355fa6
Shay Wagner <srwagner916@gmail.com>
2021-06-18 18:15:09 -0500

    wrote GET route to find one comment by ID

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,6 +12,24 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+// GET route to find on comment by ID
+//===============================================
+router.get('/:id', (req, res) => {
+  Comment.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCommentData => {
+      if (!dbCommentData) {
+        res.status(404).json({ message: "I'm sorry, no comment with this ID was found" });
+        return;
+      }
+      res.json(dbCommentData);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
 // POST route to create a comment
 //===============================================
 router.post('/', (req, res) => {
