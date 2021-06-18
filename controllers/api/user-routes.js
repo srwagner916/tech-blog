6ed83a9ab8b2a18c96469844de48f23875d8e375
Shay Wagner <srwagner916@gmail.com>
2021-06-18 16:25:18 -0500

    wrote GET route to find all Users

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -4,6 +4,16 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+// GET route to get all Users
+//===============================================
+router.get('/', (req, res) => {
+  User.findAll({}).then(dbUserData => res.json(dbUserData))
+    .catch(err => res.status(500).json(err));
+});
+
+// POST route to create new User
+//===============================================
 router.post('/', (req, res) => {
   User.create({
     username: req.body.username,
