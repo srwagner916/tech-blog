018667ae925aa614eea49e21f81d8c4f328dd60f
Shay Wagner <srwagner916@gmail.com>
2021-06-19 17:04:47 -0500

    created handlebars templates and set up in server. wrote route to pass all posts to homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,32 @@
+////=================\\\\
+///|   Home Routes   |\\\
+//||=================||\\
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+// GET route to find all posts to use on the homepage
+//====================================================
+router.get('/', (req, res) => {
+    Post.findAll({
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'text', 'createdAt'],
+          include: [
+            {
+              model: User,
+              attributes: ['username']
+            }
+          ]
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', { posts })
+      })
+      .catch(err => res.status(500).json(err));
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const sequelize = require ('./config/connection');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,9 +22,14 @@ const sess = {
 
 app.use(session(sess));
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers'));
 
